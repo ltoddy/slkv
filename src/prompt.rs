4f657570ce192310968a548b59d888a8c3617329
ltoddy <taoliu0509@gmail.com>
2019-04-01 21:33:06 +0800

    dispatch command

---
+++ src/prompt.rs
@@ -0,0 +1,27 @@
+pub fn welcome() {
+    println!("Welcome!");
+    println!("  `slkv` is a simple, lightweight key-value type database.(also is a toy.)");
+    println!("Have fun!");
+    println!();
+}
+
+pub fn help() {
+    println!("Usage:\n");
+    // TODO
+}
+
+pub fn get() {
+    // TODO
+}
+
+pub fn put() {
+    // TODO
+}
+
+pub fn delete() {
+    // TODO
+}
+
+pub fn scan() {
+    // TODO
+}
