7ba2f392e2d57ca723c9a7279782cae089a773e1
ltoddy <taoliu0509@gmail.com>
2019-04-07 15:41:54 +0800

    deal with client sended data in memory

--- slkv-server/src/main.rs
+++ slkv-server/src/main.rs
@@ -1,7 +1,45 @@
+pub mod persistence;
+pub mod storage;
+
+use std::fs::File;
 use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::path;
 
-fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+use std::path::Path;
+use storage::Storage;
+
+const ADDRESS: &str = "0.0.0.0:2333";
+const FILE_PATH: &str = "data.sldb";
+
+fn main() -> io::Result<()> {
+    let mut storage = init();
+
+    {
+        let listener = TcpListener::bind(ADDRESS)?;
+        println!("Server listening on {} ...", ADDRESS);
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => handle_client(&mut storage, stream)?,
+                Err(e) => println!("Error: {}", e),
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn init() -> Storage {
+    if Path::new(FILE_PATH).exists() {
+        Storage::load_from_file(Path::new(FILE_PATH))
+            .map_err(|err| eprintln!("Can't open {}\n{}", FILE_PATH, err))
+            .unwrap()
+    } else {
+        Storage::new()
+    }
+}
+
+fn handle_client(storage: &mut Storage, mut stream: TcpStream) -> io::Result<()> {
     let mut data = [0 as u8; 1024]; // using 1014 byte buffer
     let mut arguments = Vec::with_capacity(10);
 
@@ -24,27 +62,30 @@ fn handle_client(mut stream: TcpStream) -> io::Result<()> {
         }
     }
 
-    println!("all arguments is --> {:?}", arguments);
+    let argument = arguments
+        .iter()
+        .fold(String::with_capacity(128), |mut acc, x| {
+            acc.push_str(x.as_str());
+            acc
+        });
+    dispatch(storage, argument);
     Ok(())
 }
 
-const ADDRESS: &str = "0.0.0.0:2333";
+fn dispatch(storage: &mut Storage, argument: String) -> String {
+    let args: Vec<String> = argument[1..].split(" ").map(|s| s.into()).collect();
 
-fn main() -> io::Result<()> {
-    {
-        let listener = TcpListener::bind(ADDRESS)?;
-        println!("Server listening on 0.0.0.0:2333 ...");
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    handle_client(stream)?;
-                }
-                Err(e) => {
-                    println!("Error: {}", e);
-                }
-            }
-        }
+    if argument.starts_with("+") {
+        return storage.put(args);
+    } else if argument.starts_with("*") {
+        return storage.get(args);
+    } else if argument.starts_with("-") {
+        return storage.delete(args);
+    } else if argument.starts_with("/") {
+        // TODO
+        println!("SCAN    ==> {}", argument);
+        return String::from("Scan");
+    } else {
+        return String::from("Ok");
     }
-
-    Ok(())
 }
---
+++ slkv-server/src/storage.rs
@@ -0,0 +1,151 @@
+use std::collections::{HashMap, LinkedList};
+use std::fs::{self, File};
+use std::io;
+use std::io::{Read, Write};
+use std::path::Path;
+
+#[derive(Debug, Clone)]
+struct Entry {
+    key: String,
+    value: String,
+}
+
+impl Entry {
+    pub fn new(key: String, value: String) -> Self {
+        Entry { key, value }
+    }
+
+    pub fn to_string(&self) -> String {
+        let Self { key, value } = self;
+        format!("Entry({}, {})", key, value)
+    }
+
+    pub fn as_bytes(&self) -> Vec<u8> {
+        let Self { key, value } = self;
+
+        let mut buffer = Vec::with_capacity(self.key.len() + self.value.len() + 1);
+        buffer.append(&mut key.clone().into_bytes());
+        buffer.append(&mut value.clone().into_bytes());
+        buffer.push(10);
+        buffer
+    }
+}
+
+#[derive(Debug)]
+pub struct Storage {
+    list: LinkedList<Entry>,
+    map: HashMap<String, String>,
+}
+
+impl Storage {
+    pub fn new() -> Self {
+        Storage {
+            list: LinkedList::new(),
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn load_from_file(filename: &Path) -> io::Result<Self> {
+        let mut file = File::open(filename)?;
+        let mut buffer = String::with_capacity(1024);
+        file.read_to_string(&mut buffer)?;
+
+        let mut storage = Self::new();
+
+        let a = buffer.split("\n");
+        println!("=====> {:?}", a);
+
+        Ok(storage)
+    }
+
+    pub fn dump_to_file(&self, filename: &Path) -> io::Result<()> {
+        //        let mut file = match File::open(filename) {
+        //            Ok(file) => file,
+        //            Err(_) => File::create(filename).map_err(|err| {
+        //                eprintln!("Can't create: {}", err);
+        //                err
+        //            })?,
+        //        };
+        fs::write(filename, self.to_string())?;
+
+        Ok(())
+    }
+
+    fn as_bytes(&self) -> Vec<u8> {
+        let mut buffer = Vec::with_capacity(1024);
+
+        self.list
+            .iter()
+            .for_each(|entry| buffer.append(&mut entry.as_bytes()));
+
+        buffer
+    }
+
+    fn to_string(&self) -> String {
+        let mut buffer = String::with_capacity(1024);
+
+        self.list.iter().for_each(|entry| {
+            let Entry { key, value } = entry;
+            buffer.push_str(format!("{} {}\n", key, value).as_str());
+        });
+
+        buffer
+    }
+
+    pub fn get(&self, keys: Vec<String>) -> String {
+        println!("get request, received data: {:?}", keys);
+
+        let mut data: String = String::with_capacity(1024);
+
+        keys.iter().for_each(|key| {
+            data.push_str(
+                format!(
+                    "{} => {}\n",
+                    key,
+                    self.map.get(key).unwrap_or(&String::from("None"))
+                )
+                .as_str(),
+            )
+        });
+
+        println!("get final:====> {:?}", self);
+
+        data
+    }
+
+    pub fn put(&mut self, mut kvs: Vec<String>) -> String {
+        println!("put request, received data: {:?}", kvs);
+
+        if kvs.len() % 2 != 0 {
+            kvs.pop(); // throw away the last useless element
+        }
+
+        for index in (0..kvs.len()).step_by(2) {
+            let key = kvs[index].to_string();
+            let value = kvs[index + 1].to_string();
+            self.list.push_back(Entry::new(key.clone(), value.clone()));
+            self.map.insert(key.clone(), value.clone());
+        }
+
+        String::from("Ok\n")
+    }
+
+    pub fn delete(&mut self, keys: Vec<String>) -> String {
+        println!("delete request, received data: {:?}", keys);
+
+        keys.iter().for_each(|key| {
+            self.map.remove(key);
+            self.list = self
+                .list
+                .iter()
+                .filter(|entry| &entry.key != key)
+                .map(|entry| entry.clone())
+                .collect::<LinkedList<Entry>>();
+        });
+
+        String::from("Ok\n")
+    }
+
+    // TODO
+    // pub fn scan(&self, start: usize, end: usize) {}
+}
