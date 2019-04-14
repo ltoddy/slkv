2999e145964d335fed484b28d7269b980b7a4117
ltoddy <taoliu0509@gmail.com>
2019-04-14 19:03:06 +0800

    add main tests and annotations

--- slkv-server/src/main.rs
+++ slkv-server/src/main.rs
@@ -10,7 +10,7 @@ use storage::Storage;
 
 fn main() -> io::Result<()> {
     let mut storage = init();
-
+    // 对于服务端来说,如果出现任何重大的异常,就让服务端挂掉.
     {
         let listener = TcpListener::bind(ADDRESS)?;
         println!("Server listening on {} ...", ADDRESS);
@@ -22,6 +22,7 @@ fn main() -> io::Result<()> {
                 Err(e) => println!("Error: {}", e),
             };
         }
+        // auto drop, disconnect.
     }
 
     Ok(())
@@ -30,8 +31,7 @@ fn main() -> io::Result<()> {
 fn init() -> Storage {
     if Path::new(FILE_PATH).exists() {
         Storage::load_from_file(Path::new(FILE_PATH))
-            .map_err(|err| eprintln!("Can't open {}\n{}", FILE_PATH, err))
-            .unwrap()
+            .unwrap_or_else(|_| panic!("Can't open {}", FILE_PATH))
     } else {
         Storage::new()
     }
@@ -39,8 +39,9 @@ fn init() -> Storage {
 
 fn handle_client(storage: &mut Storage, mut stream: TcpStream) -> io::Result<()> {
     let mut data = [0 as u8; 1024]; // using 1014 byte buffer
-    let mut arguments = Vec::with_capacity(10);
+    let mut arguments = Vec::with_capacity(16);
 
+    // 从客户端接受数据
     let _ = stream
         .read(&mut data)
         .map(|size| {
@@ -59,6 +60,7 @@ fn handle_client(storage: &mut Storage, mut stream: TcpStream) -> io::Result<()>
         });
     let result: String = format!("{}!", dispatch(storage, argument));
 
+    // 讲客户端发送数据处理之后的结果返回给客户端
     stream.write(result.as_bytes()).map_err(|err| {
         eprintln!("can't send data to client: {}", err);
         err
@@ -67,6 +69,7 @@ fn handle_client(storage: &mut Storage, mut stream: TcpStream) -> io::Result<()>
     Ok(())
 }
 
+// 调度客户端发送过来的数据
 fn dispatch(storage: &mut Storage, argument: String) -> String {
     let args: Vec<String> = argument[1..].split(' ').map(|s| s.into()).collect();
 
@@ -80,6 +83,37 @@ fn dispatch(storage: &mut Storage, argument: String) -> String {
         storage.scan(args)
     } else {
         // ignore wrong command.
+        // 因为客户端已经做过检查,所以这里是走不到的.
         String::new()
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::dispatch;
+    use super::Storage;
+
+    #[test]
+    pub fn test_dispatch_arguments() {
+        let mut storage = Storage::new();
+        // put
+        let argument = String::from("+key1 value1");
+        let res = dispatch(&mut storage, argument);
+        assert_eq!(res, "Ok\n");
+
+        // get
+        let argument = String::from("*key1 non-exist");
+        let res = dispatch(&mut storage, argument);
+        assert_eq!(res, "key1 => value1\nnon-exist => None\n");
+
+        // delete
+        let argument = String::from("-key1");
+        let res = dispatch(&mut storage, argument);
+        assert_eq!(res, "Ok\n");
+
+        // scan
+        let argument = String::from("/0 10");
+        let res = dispatch(&mut storage, argument);
+        assert_eq!(res, "");
     }
 }
